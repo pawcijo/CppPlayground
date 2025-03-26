f8de05ec7490e72a41630790a11226c1e52cf375
pawcijo <pawcijo@gmail.com>
2025-03-26 01:18:41 +0100

    new style test commit

--- src/Common/DemoExample.hpp
+++ src/Common/DemoExample.hpp
@@ -4,40 +4,33 @@
 #include "Common/DemoBase.hpp"
 
 #include <iostream>
-#include <vector>
 #include <utility>
+#include <vector>
 
-//Copy this code to create new example
+// Copy this code to create new example
 class DemoExample : public DemoBase
 {
 public:
-    DemoExample()
-    {
-        mName = "DemoExample";
-        mNotes = {
-            {"Paragraph:", "bla bla bla bla bla notes"}};
-    };
-    ~DemoExample() = default;
+  DemoExample()
+  {
+    mName = "DemoExample";
+    mNotes = { { "Paragraph:", "bla bla bla bla bla notes" } };
+  };
+  ~DemoExample() = default;
 
-    void PrintNotes() override
-    {
-        PrintName();
+  void PrintNotes() override
+  {
+    PrintName();
 
-        for (const auto &pair : mNotes)
-        {
-            std::cout << BOLD_TEXT_START << pair.first << TEXT_FORMAT_CLEAR << std::endl
-                      << pair.second << std::endl
-                      << std::endl;
-        }
+    for (const auto& pair : mNotes) {
+      std::cout << BOLD_TEXT_START << pair.first << TEXT_FORMAT_CLEAR
+                << std::endl
+                << pair.second << std::endl
+                << std::endl;
     }
+  }
 
-    void ShowExample() override
-    {
-        PrintNotes();
-    }
+  void ShowExample() override { PrintNotes(); }
 
-    void ShowDemo() override
-    {
-        ShowExample();
-    };
+  void ShowDemo() override { ShowExample(); };
 };
