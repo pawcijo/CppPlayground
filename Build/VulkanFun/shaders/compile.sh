e674451072f50bfbe28e6d0e5e6f634335073f74
Pawcijo <pawcijo@gmail.com>
2025-08-17 19:57:33 +0200

    macos vulkan window fix (portability extensions)

--- Build/VulkanFun/shaders/compile.sh
+++ Build/VulkanFun/shaders/compile.sh
@@ -1,2 +1,2 @@
-/home/pawcijo/VulkanSdk/1.4.321.1/x86_64/bin/glslc shader.vert -o vert.spv
-/home/pawcijo/VulkanSdk/1.4.321.1/x86_64/bin/glslc shader.frag -o frag.spv
+glslc shader.vert -o vert.spv
+glslc shader.frag -o frag.spv
